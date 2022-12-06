c60555733ea85e69e6f3a60084204a92ea3b74bf
Mor Ezra <mor3311@gmail.com>
2022-12-06 15:06:31 +0200

    define mongodb connection

---
+++ backend/dao/mongo-connection-wrapper.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require('mongodb');
+const uri = 'mongodb+srv://mor3311:tuniFg1v9Opqb4fw@cluster0.ldhcc3y.mongodb.net/test';
+const client = new MongoClient(uri);
+let connection;
+
+try {
+    // Connect to the MongoDB cluster
+    client.connect();
+    connection = mongoClient.db("persons");
+} catch (e) {
+    console.error(e);
+} finally {
+    client.close();
+}
+
+function getDB() {
+    return connection;
+}
+
+module.exports = {
+    getDB
+};
